63b8b10be4cb5d824102eddd4311779a940d4836
David Halewood <davidnhalewood@hotmail.com>
2017-10-24 16:18:25 +0100

    deleting comments

--- jasmine-standalone-2/spec/airportSpec.js
+++ jasmine-standalone-2/spec/airportSpec.js
@@ -20,34 +20,3 @@ describe ('Airport',function(){
     expect(airport.allPlanes()).not.toContain(plane);
   });
 });
-
-
-
-
-// function Sheep(name) {
-//   this._name = name;
-// };
-
-// Sheep.prototype.sayName = function() {
-//   return this._name;
-// };
-
-// var barry = new Sheep(‘Barry’);
-
-// function Pen() {
-//   this._allSheep = [];
-// };
-
-// Pen.prototype.herd = function(sheep) {
-//   this._allSheep.push(sheep);
-// };
-
-// Pen.prototype.rollCall = function() {
-//   this._allSheep.forEach(function(sheep){
-//     console.log(sheep.sayName());
-//   });
-// };
-
-// var pen = new Pen();
-// pen.herd(barry);
-// pen.rollCall();
